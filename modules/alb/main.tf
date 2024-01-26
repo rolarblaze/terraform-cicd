9a40acfcde15c4256c3ca0ca380f2f44954e9602
Roland Joseph <wordofwisdom_2@yahoo.com>
2024-01-26 23:13:08 +0100

    https

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -16,8 +16,8 @@ resource "aws_lb" "alb" {
 #Listener
 resource "aws_lb_listener" "lister" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
+  port              = "443"
+  protocol          = "HTTPS"
 
   default_action {
     type             = "forward"
@@ -28,8 +28,8 @@ resource "aws_lb_listener" "lister" {
 #Target Group
 resource "aws_lb_target_group" "tg" {
   name     = "lbtg"
-  port     = 80
-  protocol = "HTTP"
+  port     = 443
+  protocol = "HTTPS"
   vpc_id   = var.vpc_id
 }
 
@@ -38,5 +38,5 @@ resource "aws_lb_target_group_attachment" "tga" {
   count = length(var.instances)
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = var.instances[count.index]
-  port             = 80
+  port             = 443
 }
